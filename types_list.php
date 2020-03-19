286a36e359340b756e32a5d4a436b932fcd35469
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-19 02:51:21 -0500

    Update types_list.php

--- types_list.php
+++ types_list.php
@@ -1,60 +1,44 @@
 <?php include 'view/header.php'; ?>
-    <main>
-        <section>
-            <?php if ( sizeof($categories) != 0) { ?>
-                <form action="." method="get" id="category_selection">
-                <label>Category:</label>
-                <select name="category_id">
-                    <option value="0">View All Categories</option>
-                    <?php foreach ($categories as $category) : ?>
-                        <option value="<?php echo $category['categoryID']; ?>">
-                            <?php echo $category['categoryName']; ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select> 
-                <input type="submit" value="Submit" class="button blue">
-                </form>
-            <?php } ?>
+<main>
+    <h2>Types List</h2>
+    <section>
+        <table>
+            <tr>
+                <th colspan="2">Name</th>
+            </tr>        
+            <?php foreach ($types as $type) : ?>
+            <tr>
+                <td><?php echo $type['typeName']; ?></td>
+                <td>
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="delete_type">
+                        <input type="hidden" name="type_id"
+                            value="<?php echo $type['type_id']; ?>"/>
+                        <input type="submit" value="Remove" class="button red" />
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>    
+        </table>
+    </section>
+    <section>
+        <h2>Add Type</h2>
+        <form action="." method="post" id="add_vehicle_form">
+            <input type="hidden" name="action" value="add_vehicle">
 
-            <?php if( sizeof($items) != 0 ) { ?>
-                <div id="table-overflow">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th colspan="2">Category</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($items as $item) : ?>
-                            <tr>
-                                <td><?php echo $item['Title']; ?></td>
-                                <td><?php echo $item['Description']; ?></td>
-                                <?php if ($item['categoryName'] == null || $item['categoryName'] == false) { ?>
-                                    <td>None</td>
-                                <?php } else { ?>
-                                    <td><?php echo $item['categoryName']; ?></td>
-                                <?php } ?>
-                                <td>
-                                    <form action="." method="post">
-                                        <input type="hidden" name="action" value="delete_item">
-                                        <input type="hidden" name="item_id"
-                                            value="<?php echo $item['ItemNum']; ?>">
-                                        <input type="submit" value="Remove" class="button red">
-                                    </form>
-                                </td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-                <p><a href="?action=show_add_form">Click here</a> to add a new item to the list.</p>     
-            <?php } else { ?>
-                <p>No to do list items exist yet. <a href="?action=show_add_form">Click here</a> to add an item.</p>     
-            <?php } ?>
-            <br>
-            <p><a href="index.php?action=list_categories">View/Edit Categories</a></p>
-        </section>
-    </main>
-<?php include 'view/footer.php'; ?>
+            <label>Name:</label>
+            <input type="text" name="type_name" max="20" required><br>
+
+            <label>&nbsp;</label>
+            <input id="add_type_button" type="submit" class="button blue" value="Add Type"><br>
+        </form>
+    </section>
+    <section>
+        <p><a href="index.php">View Vehicles List</a></p>
+    </section>
+</main>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Zippy Used Autos</p>
+</footer>
+</body>
+</html>
