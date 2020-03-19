563a88d9f3a6d3f3be0c21973260faad876de5d9
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-19 18:01:13 -0500

    first commit

--- model/types_db.php
+++ model/types_db.php
@@ -1,49 +1,31 @@
-<?php 
-    function get_items_by_category($category_id) {
-        global $db;
-        if ($category_id == NULL || $category_id == FALSE) {
-            $query = 'SELECT T.ItemNum, T.Title, T.Description, C.categoryName FROM todoitems T LEFT JOIN categories C ON T.categoryID = C.categoryID ORDER BY C.categoryID';
-        } else {
-            $query = 'SELECT T.ItemNum, T.Title, T.Description, C.categoryName FROM todoitems T LEFT JOIN categories C ON T.categoryID = C.categoryID WHERE T.categoryID = :category_id ORDER BY ItemNum';
-        }
-        $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->execute();
-        $items = $statement->fetchAll();
-        $statement->closeCursor();
-        return $items;
-    }
-
-    function get_item($item_id) {
-        global $db;
-        $query = 'SELECT * FROM todoitems WHERE ItemNum = :item_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':item_id', $item_id);
-        $statement->execute();
-        $item = $statement->fetch();
-        $statement->closeCursor();
-        return $item;
-    }
-
-    function delete_item($item_id) {
-        global $db;
-        $query = 'DELETE FROM todoitems WHERE ItemNum = :item_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':item_id', $item_id);
-        $statement->execute();
-        $statement->closeCursor();
-    }
-
-    function add_item($category_id, $title, $description) {
-        global $db;
-        $query = 'INSERT INTO todoitems (Title, Description, categoryID)
-              VALUES
-                 (:title, :descr, :categoryID)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':title', $title);
-        $statement->bindValue(':descr', $description);
-        $statement->bindValue(':categoryID', $category_id);
-        $statement->execute();
-        $statement->closeCursor();
-    }
+<?php
+  //function to get types sorted by type_id
+  function get_types() {
+    global $db;
+    $query = 'SELECT * FROM types ORDER BY type_id';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $types = $statement->fetchAll();
+    $statement->closeCursor();
+    return $types;
+  }
+  //function to add a type
+  function add_type($type_name) {
+    global $db;
+    $query = 'INSERT INTO types (type_name) VALUES (:type_name';
+    $statement - $db->prepare($query);
+    $statement->bindValue(':type_name', $type_name);
+    $statement->execute();
+    $statement->closeCursor();
+  }
+  //function to delete a type   
+  function delete_type($type_id) {
+    global $db;
+    $query = 'DELETE FROM types WHERE type_id = :type_id';
+    $statement - $db->prepare($query);
+    $statement->bindValue(':type_id', $type_id);
+    $statement->execute();
+    $statement->closeCursor();
+  }
+  
 ?>
