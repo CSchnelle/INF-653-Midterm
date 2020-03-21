1b7646b466940361ac75763b1af422b10f17686d
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-20 22:37:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,7 @@
         }
     }
   if ($action == 'get_vehicles') {
-        $type_id = filter_input(INPUT_GET, 'type_id', FILTER_VALIDATE_INT);
-        $class_id = filter_input(INPUT_GET, 'class_id', FILTER_VALIDATE_INT);
-        $
+		$vehicles = get_vehicles();
         include('vehicle_list.php');
      //list makes
   } else if ($action == 'get_makes'){
@@ -26,47 +24,46 @@
   } else if ($action == 'show_add_form') {
       $vehicles = get_vehicles();
       include('add_vehicle_form.php');
-  //add vehicle
-  } else if ($action == 'add_vehicle'); {
-      $type_id = filter_input(INPUT_POST, 'type_id', FILTER_VALIDATE_INT);
-      $class_id = filter_input(INPUT_POST, 'class_id', FILTER_VALIDATE_INT);
-      $make = filter_input(INPUT_POST, 'make');
-      $model = filter_input(INPUT_POST, 'model');
-      $year = filter_input(INPUT_POST, 'year', FILTER_VALIDATE_INT);
-      $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_DECIMAL);
-      if($type_id == NULL|| $class_id == NULL || $make == NULL || $model == NULL || $year == NULL || $price == NULL) {
-          $error = "Invalid vehicle data. Check all fields and try again.";
-          include('errors/errors.php');
-      } else {
-          add_vehicle($type_id, $class_id, $make, $model, $year, $price);
-          header("Location: .?action=list_vehicles");
-   //delete vehicle	      
-  } else if ($action == 'delete_vehicle'); {
+  //sort by price
+  } else if ($action == 'get_by_price') {
+	$price = filter_input(INPUT_GET, 'price', FILTER_VALIDATE_INT);
+	include('vehicle_list.php');
+  //sort by year
+  } else if ($action == 'get_by_year') {
+ 	$year = filter_input(INPUT_GET, 'year', FILTER_VALIDATE_INT);
+	include('vehicle_list.php');
+  //sort by class		      
+  } else if ($action =='get_by_class') {
+        $classes = filter_input(INPUT_GET, 'class_name');
+	include('vehicle_list.php');
+  //sort by type		      
+  } else if($action =='get_by_type'){
+	$types = filter_input(INPUT_GET, 'type_name', FILTER_VALIDATE_INT);
+	include('vehicle_list.php');
+ 	   
+    //delete vehicle	      
+  } else if ($action == 'delete_vehicle') {
 	      $vehicle_id = filter_input(INPUT_POST, 'vehicle_id', FILTER_VALIDATE_INT);
 	      if($vehicle_id == NULL) {
 		      $error = "There are no vehicles to delete.";
 		      include('errors/errors.php');
-	      } else {
+	      } else 
 		      delete_vehicle($vehicle_id);
-		      header("Location .?action=list_vehicles");
-  //sort by price		      
-  } else if ($action == 'get_by_price'); {
-	$price = filter_input(INPUT_GET, 'price', FILTER_VALIDATE_INT);
-	include(vehicle_list.php);
-  //sort by year		      
-  } else if ($action == 'get_by_year'); {
- 	$year = filter_input(INPUT_GET, 'year', FILTER_VALIDATE_INT);
-	include(vehicle_list.php);
-  //sort by class		      
-  } else if ($action =='get_by_class'); {
-        $classes = filter_input(INPUT_GET, 'class_name');
-	include(vehicle_list.php);
-  //sort by type		      
-  } else ($action =='get_by_type');{
-	types = filter_input(INPUT_GET, 'type_name', FILTER_VALIDATE_INT);
-	include(vehicle_list.php);
- 	 }
+		      header("Location: .?action=list_vehicles"); 
+  //add vehicle
+  } else ($action == 'add_vehicle') {
+      $type_name = filter_input(INPUT_POST, 'type_name');
+      $class_name = filter_input(INPUT_POST, 'class_name');
+      $make = filter_input(INPUT_POST, 'make');
+      $model = filter_input(INPUT_POST, 'model');
+      $year = filter_input(INPUT_POST, 'year');
+      $price = filter_input(INPUT_POST, 'price');
+		if($type_name == NULL|| $class_name == NULL || $make == NULL || $model == NULL || $year == NULL || $price == NULL) {
+          $error = "Invalid vehicle data. Check all fields and try again.";
+          include('errors/errors.php');
+		} else 
+          add_vehicle($type_name, $class_name, $make, $model, $year, $price);
+          header("Location: .?action=list_vehicles");
   }
-				   
-?>          
+?>         
       
