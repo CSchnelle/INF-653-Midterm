c694d944f3811f4dadd7f0da5b115ad7ab8a53f3
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 00:07:19 -0500

    Update vehicle_list.php

--- vehicle_list.php
+++ vehicle_list.php
@@ -1,50 +1,97 @@
-<?php include 'view/header.php'; ?>
 <main>
-    <h2>Vehicle List</h2>
-    <section>
-        <table>
-            <tr>
-                <th colspan="2"></th>
-                <th>Make</th>
-                <th>Model</th>
-                <th>Year</th>
-                <th>Price</th>
-                <th>Type</th>
-                <th>Class</th>
-            </tr>        
-            <?php foreach ($vehicless as $vehicle) : ?>
-            <tr>
-                <td> <?php echo $vehicle['make']; ?></td>
-                <td> <?php echo $vehicle['model']; ?></td>
-                <td> <?php echo $vehicle['year']; ?></td>
-                <td> <?php echo $vehicle['price']; ?></td>
-                <td> <?php echo $vehicle['type_name']; ?></td>
-                <td> <?php echo $vehicle['class_name']; ?></td>
-             
-                </td>
-            </tr>
-            <?php endforeach; ?>    
-        </table>
-    </section>
-    <section>
-        <h2>Sort by</h2>
-        <form action="." method="get" id="get_by">
-                <select name = "action">
-                    <option value="get_by_price">Price</option>
-                    <option value="get_by_year">Year</option>
-                    <option value="get_by_type">Type</option>
-                    <option value="get_by_class">Class</option>
-            </select>
-            <input type="submit" value="submit"/>
+    <nav>
+        <form action="." method="get" id="make_selection">
+            <section id="dropmenus">
+                <?php if ( sizeof($makes) != 0) { ?>
+                    
+                    <label>Make:</label>
+                    <select name="make">
+                        <option value="0">View All Makes</option>
+                        <?php foreach ($makes as $make) : ?>
+                            <option value="<?php echo $make['make']; ?>">
+                                <?php echo $make['make']; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select> 
+                <?php } ?>
+
+                <?php if ( sizeof($types) != 0) { ?>
+                    <label>Types:</label>
+                    <select name="type_id">
+                        <option value="0">View All Types</option>
+                        <?php foreach ($types as $type) : ?>
+                            <option value="<?php echo $type['typeID']; ?>">
+                                <?php echo $type['typeName']; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select> 
+                <?php } ?>
+
+                <?php if ( sizeof($classes) != 0) { ?>
+                    <label>Class:</label>
+                    <select name="class_id">
+                        <option value="0">View All Classes</option>
+                        <?php foreach ($classes as $class) : ?>
+                            <option value="<?php echo $class['classID']; ?>">
+                                <?php echo $class['className']; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select> 
+                    
+                <?php } ?>
+            </section>
+            <section id="sortBy">
+                <div>
+                    <span>Sort by: </span>
+                    <input type="radio" id="sortByPrice" name="sort" value="price" checked>
+                    <label for="sortByPrice">Price</label> 
+                    <input type="radio" id="sortByYear" name="sort" value="year">
+                    <label for="sortByYear">Year</label>
+                    <input type="submit" value="Submit Search" class="button blue button-slim">
+                </div>
+            </section>
         </form>
-        
+    </nav>
     <section>
-        <p><a href="index.php">View Vehicles List</a></p>
+        <?php if( sizeof($vehicles) != 0 ) { ?>
+            <div id="table-overflow">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Make</th>
+                            <th>Model</th>
+                            <th>Type</th>
+                            <th>Class</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($vehicles as $vehicle) : ?>
+                        <tr>
+                            <td><?php echo $vehicle['vYear']; ?></td>
+                            <td><?php echo $vehicle['make']; ?></td>
+                            <td><?php echo $vehicle['model']; ?></td>
+                            <?php if ($vehicle['typeName'] == null || $vehicle['typeName'] == false) { ?>
+                                <td>None</td>
+                            <?php } else { ?>
+                                <td><?php echo $vehicle['typeName']; ?></td>
+                            <?php } ?>
+                            <?php if ($vehicle['className'] == null || $vehicle['className'] == false) { ?>
+                                <td>None</td>
+                            <?php } else { ?>
+                                <td><?php echo $vehicle['className']; ?></td>
+                            <?php } ?>
+                            <td><?php echo "$".number_format($vehicle['price'], 2); ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>  
+        <?php } else { ?>
+            <p>
+                There are no matching vehicles in Zippy&apos;s inventory. 
+            </p>     
+        <?php } ?>
     </section>
 </main>
-<footer>
-    <p>&copy; <?php echo date("Y"); ?>Zippy Used Autos</p>
-</footer>
-</body>
-</html>
-
