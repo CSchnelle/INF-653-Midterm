8bc54fc81336eba4d916d1c863dfd6668691153e
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 00:02:44 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -3,6 +3,8 @@
 
 <!-- the head section -->
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zippy Used Autos</title>
     <link rel="stylesheet" type="text/css" href="view/css/main.css" />
 </head>
