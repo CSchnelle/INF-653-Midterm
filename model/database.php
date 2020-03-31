11c837ead3e60b184fba9bd6bd9b405efb3a4981
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 00:24:53 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,10 +1,21 @@
 <?php
-    $dsn = 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f33okin59mld7tzf';
+    //local development server connection
+   // $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
+   // $username = 'root';
+    //$password = 'pa55word';
+
+    // Heroku connection
+    
+    $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f33okin59mld7tzf';
     $username = 'hu0wlnc966uyzjy4';
     $password = 'cmtbdgfv39s2i8o1';
-
+    
     try {
+        //local development server connection
+        //if using a $password, add it as 3rd parameter
         $db = new PDO($dsn, $username);
+
+        // Heroku connection
         //$db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
